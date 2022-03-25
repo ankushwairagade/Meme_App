7c9ff289daffbfc572108f42cd66d167f93604bb
AnkushWairagade <ankushwairagade88@gmail.com>
2022-03-25 09:06:28 +0530

    commit with apk

--- app/src/main/java/com/ankush/meme/MainActivity.java
+++ app/src/main/java/com/ankush/meme/MainActivity.java
@@ -1,10 +1,19 @@
 package com.ankush.meme;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,29 +24,78 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Button share,next,download;
+    String imageurl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next =findViewById(R.id.next);
+        progressBar = findViewById(R.id.progressBar);
+        share = findViewById(R.id.share);
+        download = findViewById(R.id.download);
+        LoadMeme();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoadMeme();
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                Uri screenshotUri = Uri.parse(imageurl);
+                try {
+                    InputStream stream = getContentResolver().openInputStream(screenshotUri);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+                startActivity(Intent.createChooser(intent, "Share image using"));
 
-       LoadMeme();
+            }
+        });
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private  void LoadMeme()
     {
-
+        progressBar.setVisibility(View.VISIBLE);
 // ...
          ImageView imageView =(ImageView)findViewById(R.id.imageView);
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://meme-api.herokuapp.com/gimme";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
@@ -45,10 +103,28 @@ public class MainActivity extends AppCompatActivity {
                        // textView.setText("Response: " + response.toString());
 
                         try {
+
                             String url =response.getString("url");
+                            imageurl =url;
                             System.out.println(url);
-                            Glide.with(MainActivity.this).load(url).into(imageView);
+                            Glide.with(MainActivity.this).load(url).listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    progressBar.setVisibility(View.GONE);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    progressBar.setVisibility(View.GONE);
+
+                                    return false;
+                                }
+                            }).into(imageView);
+                            progressBar.setVisibility(View.INVISIBLE);
+
                         } catch (JSONException e) {
+                            progressBar.setVisibility(View.VISIBLE);
                             e.printStackTrace();
                         }
 
@@ -67,11 +143,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
     }
 
-    public void next(View view) {
-        LoadMeme();
-    }
+
+
 }
